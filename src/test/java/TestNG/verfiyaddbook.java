e61cea6033d0e4b87d12b2367639c398f38cd524
preethi <preethi@DESKTOP-7GP1KNR>
2020-02-02 21:49:47 +0530

    Final chnages

---
+++ src/test/java/TestNG/verfiyaddbook.java
@@ -0,0 +1,41 @@
+package TestNG;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class verfiyaddbook {
+	   WebDriver driver;
+	@BeforeMethod
+	public void launch() {
+	     System.setProperty("webdriver.chrome.driver","chromedriver.exe");
+	   driver = new ChromeDriver();
+	  driver.manage().window().maximize();
+	  driver.manage().timeouts().implicitlyWait(30,TimeUnit.SECONDS);
+	  driver.get("http://52.66.236.15:32768/addressbook/");
+	
+	}
+	
+  @Test
+  public void verfiy() {
+	 
+	  driver.findElement(By.xpath("//div[@class='v-button v-widget']")).click();
+	  driver.findElement(By.id("gwt-uid-5")).sendKeys("Edureka");
+	  driver.findElement(By.id("gwt-uid-7")).sendKeys("Selenium");
+	  driver.findElement(By.id("gwt-uid-9")).sendKeys("9876543210");
+	  driver.findElement(By.id("gwt-uid-11")).sendKeys("devops@gmail.com");
+	  driver.findElement(By.id("gwt-uid-13")).sendKeys("06/11/1986");
+	  driver.findElement(By.xpath("//div[@class='v-button v-widget primary v-button-primary']")).click();
+	  
+  }
+  
+  @AfterMethod
+  public void close() {
+	  driver.close();
+  }
+}
